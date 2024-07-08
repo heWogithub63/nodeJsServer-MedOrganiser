c0549d01fe03748ab634f06fc4b6ef8b76383f47
heWogithub63 <heworring@gmail.com>
2024-07-08 10:10:15 -0400

    next version

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const port =  process.env.PORT || 3030;
 
-var arr;
+var obj;
+var arrk,arrv;
 var resend;
 var commentsCount = 0;
 var complainsCount = 0;
@@ -23,10 +24,11 @@ app.use(cors())
 app.post('/medOrganiser', (req, res) =>{
 	const data = req.body;
     resend = res;
-        
-	arr = data.toString().split(',');
+    obj = data;
+    arrk = Object.keys(data);
+    arrv = Object.values(data);
 
-    console.log("-->-"+arr);
+    console.log("-->"+arrk +"-:-"+arrv);
 	requestPost().catch(console.error);
 })
 
@@ -39,7 +41,7 @@ app.listen(port, ()=>{
 async function requestPost() {
 	const uri = "mongodb+srv://wh:admin01@cluster0.kmwrpfb.mongodb.net/?retryWrites=true&w=majority";
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        const collection = client.db("MedOrganiser").collection(arr[1]);
+        const collection = client.db("MedOrganiser").collection(arrv[1]);
         try {
                 await client.connect((err) => {
 		  if (err) {
@@ -62,56 +64,45 @@ async function requestPost() {
                    
 }
 
-function createDocument(filds, i) {
-  var value = arr[i].split('°');
-  for(var n=0;n<filds.length;n++)
-      console.log(filds[n]+" : "+value[n]);
-  var obj = {};
-      filds.forEach((item, index) => {
-
-         obj[item] = value[index];
-
-      });
-
-  return obj;
-}
 
 async function read_write_Comments (collection) {
-      if(arr[0] == 'Deploy') {
-         var transfer ="";
-         var filds = arr[2].split('°');
-           for(var i=3;i<arr.length;i++) {
-                 await collection
-                            .insertOne(createDocument(filds, i));
-           }
+
+      if(arrv[0] == 'Deploy') {
+           var transfer ="";
+               await collection
+                      .insertOne(obj);
+
            transfer =  'Rückmeldung transfer succesfull -->completed';
            resend.status(200).json({body: JSON.stringify(transfer)});
+      }
 
-      }  if(arr[0].startsWith('Request')) {
+      if(arrv[0].startsWith('Request')) {
                 var transfer ="";
                 var list;
 
             try {
-                if(arr[0].endsWith('pat')) {
+                if(arrv[0].endsWith('pat')) {
+
                     await collection
-                          .find({VersicherungsNummer: arr[2]})
+                          .find({VersicherungsNummer: arrv[2]})
                           .forEach(function(result){
                                     transfer =  transfer + result.VersicherungsStatus+'°'+result.Name+'°'+result.Geburtsdatum+'°'+result.Wohnort + '-->';
                           })
-                } else if(arr[0].endsWith('plz')) {
-                   await collection
-                         .find({ PLZ_Ort: {$regex: arr[2], $options: "i" } })
-                         .forEach(function(result){
-                                   if(result.PLZ_Ort.startsWith(arr[2]) || result.PLZ_Ort.endsWith(arr[2]))
-                                      transfer =  transfer + result.PLZ_Ort + '-->';
-                         })
-                } else if(arr[0].endsWith('medE')) {
-                   console.log(arr[2]+ "-----" + arr[3])
-                   await collection
-                         .find({ $and: [{Addresse: {$regex: arr[2], $options: "i" }}, {Fachbereich: {$regex: arr[3], $options: "i" } } ] })
-                         .forEach(function(result){
-                                  transfer =  transfer + result.Name +'°'+  result.Telephon +'°'+ result.Addresse +'°'+ result.Kassenzulassung +'°'+ result.Fachbereich + '-->';
-                         })
+                } else if(arrv[0].endsWith('plz')) {
+
+                    await collection
+                          .find({ PLZ_Ort: {$regex: arrv[2], $options: "i" } })
+                          .forEach(function(result){
+                                    if(result.PLZ_Ort.startsWith(arrv[2]) || result.PLZ_Ort.endsWith(arrv[2]))
+                                       transfer =  transfer + result.PLZ_Ort + '-->';
+                          })
+                } else if(arrv[0].endsWith('medE')) {
+
+                    await collection
+                          .find({ $and: [{Addresse: {$regex: arrv[2], $options: "i" }}, {Fachbereich: {$regex: arrv[3], $options: "i" } } ] })
+                          .forEach(function(result){
+                                   transfer =  transfer + result.Name +'°'+  result.Telephon +'°'+ result.Addresse +'°'+ result.Kassenzulassung +'°'+ result.Fachbereich + '-->';
+                          })
                 }
 
                 resend.status(200).json({body: JSON.stringify(transfer.substring(0,transfer.length -3))});
